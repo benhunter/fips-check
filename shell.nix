15659c8fe2db208b13a1080fe4d3e5f60d7a8130
Ben Hunter <code@benhunter.me>
2024-11-25 02:04:17 +0000

    Nix shell: use zsh with my dotfiles

--- shell.nix
+++ shell.nix
@@ -1,16 +1,35 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
+with pkgs;
+
+mkShell {
   buildInputs = [
-    pkgs.openssl        # OpenSSL library
-    pkgs.pkg-config     # Helps cargo find OpenSSL
-    pkgs.perl           # Required by OpenSSL build scripts
-    pkgs.rustc          # Rust compiler
-    pkgs.cargo          # Rust package manager
+    # Rust
+    pkgs.cargo
+    pkgs.rustc
+
+    # OpenSSL
+    pkgs.openssl
+    pkgs.pkg-config
+    pkgs.perl
+
+    # Shell
+    pkgs.zsh
+    pkgs.zsh-powerlevel10k
+    pkgs.oh-my-zsh
+    # Include any additional dependencies required by your Zsh configuration
+    pkgs.git
+    pkgs.curl
   ];
 
-  # Environment variables to help cargo and pkg-config locate OpenSSL
+  # Environment variables for OpenSSL
   OPENSSL_DIR = "${pkgs.openssl.dev}";
   OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
   OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+
+  shellHook = ''
+    export SHELL=${pkgs.zsh}/bin/zsh
+    export NIX_SHELL_PRESERVE_PROMPT=1
+    exec ${pkgs.zsh}/bin/zsh -l
+  '';
 }
